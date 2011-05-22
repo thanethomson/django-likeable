8f1e3500df6c169d8843227450225c25767897e6
Thane Thomson <thane@praekelt.com>
2011-05-22 11:19:48 +0200

    added names for the urls

--- likeable/urls.py
+++ likeable/urls.py
@@ -9,8 +9,8 @@ from views import like_ajax, like_noajax, like_autodetect
 
 
 urlpatterns = patterns('',
-    (r'^(?P<content_type_id>\d+)/(?P<object_id>\d+)', like_autodetect),
-    (r'^noajax/(?P<content_type_id>\d+)/(?P<object_id>\d+)', like_noajax),
-    (r'^ajax/(?P<content_type_id>\d+)/(?P<object_id>\d+)', like_ajax),
+    url(r'^(?P<content_type_id>\d+)/(?P<object_id>\d+)', like_autodetect, name='likeable_like'),
+    url(r'^noajax/(?P<content_type_id>\d+)/(?P<object_id>\d+)', like_noajax, name='likeable_noajax_like'),
+    url(r'^ajax/(?P<content_type_id>\d+)/(?P<object_id>\d+)', like_ajax, name='likeable_ajax_like'),
 )
 
